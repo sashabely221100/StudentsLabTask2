b1bc00b6b2f861ec6b3f0d81c7d2689bf03b028f
sashabely221100 <sashabelyy2012@gmail.com>
2021-07-20 21:06:58 +0300

    small fixes

--- js/task1.js
+++ js/task1.js
@@ -54,65 +54,69 @@ ArrayProcessingTool.getMaxSubSumOn2 = function getMaxSubSumOn2(array) {
     return maximumSum;
 };
 
-ArrayProcessingTool.Search = function Search(array) {
-    let arr = numbers.split(",");
-    let max = arr[0];
-    let min = arr[0];
-
-    for (let num of arr) {
-        if (Number(num) < min)
-            min = num
-        if (Number(num) > max)
-            max = num;
-
-
-    }
-
-    return max + " " +
-        min;
-}
-ArrayProcessingTool.Selection = function Selection(array) {
-
-}
 
 
 
 
 window.onload = function() {
-    let inputButton = document.querySelector('.subtask__button');
-    let input = document.querySelector('.subtask__input');
-    // присвоить переменной checkbox
+        let inputButton = document.querySelector('.subtask__button');
+        let input = document.querySelector('.subtask__input');
+        // присвоить переменной checkbox
 
 
 
 
-    inputButton.onclick = function() {
-        let separator = ",";
-        let arrayFromString = input.value.split(separator);
-        console.log(arrayFromString);
-        for (let index = 0; index < arrayFromString.length; index++) {
-            arr.push(Number.parseInt(arrayFromString[index]));
+        inputButton.onclick = function() {
+            // let separator = ",";
+            let arrayFromString = input.value.split(separator);
+            console.log(arrayFromString);
+            for (let index = 0; index < arrayFromString.length; index++) {
+                arr.push(Number.parseInt(arrayFromString[index]));
+            }
+            console.log(arr);
+
+            input.value = ArrayProcessingTool.getMaxSubSumOn(arr);
+        };
+
+
+
+        ArrayProcessingTool.Search = function Search(inputString) {
+            //!переписать под кнопочки radiobutton (+.value)
+            //!повесить eventListener на кнопку чтоб слушала состояние radiobutton  
+            let arr = inputString.split(",");
+            let max = arr[0];
+            let min = arr[0];
+
+            for (let num of arr) {
+                if (Number(num) < min)
+                    min = num
+                if (Number(num) > max)
+                    max = num;
+
+
+            }
+
+            return max + " " + min;
         }
-        console.log(arr);
-
-        input.value = ArrayProcessingTool.getMaxSubSumOn(arr);
-    };
+        ArrayProcessingTool.Selection = function Selection(array) {};
 
 
 
 
-    // console.log(ArrayProcessingTool.getMaxSubSumOn([-1, 2, 3, -9])); // 5
+        console.log(ArrayProcessingTool.Search("-1, 2, 3, -9, 11"));
+
+    }
+    //min + max
     // console.log(ArrayProcessingTool.getMaxSubSumOn([-1, 2, 3, -9, 11])); // 11
     // console.log(ArrayProcessingTool.getMaxSubSumOn([-2, -1, 1, 2])); // 3
     // console.log(ArrayProcessingTool.getMaxSubSumOn([1, 2, 3])); // 6
     // console.log(ArrayProcessingTool.getMaxSubSumOn([100, -9, 2, -3, 5])); // 100
 
-    // console.log("");
-    // console.log("");
+// console.log("");
+// console.log("");
 
-    // console.log(ArrayProcessingTool.getMaxSubSumOn2([-1, 2, 3, -9])); // 5
-    // console.log(ArrayProcessingTool.getMaxSubSumOn2([-1, 2, 3, -9, 11])); // 11
-    // console.log(ArrayProcessingTool.getMaxSubSumOn2([-2, -1, 1, 2])); // 3
-    // console.log(ArrayProcessingTool.getMaxSubSumOn2([1, 2, 3])); // 6
-    // console.log(ArrayProcessingTool.getMaxSubSumOn2([100, -9, 2, -3, 5])); // 100
-}
+// console.log(ArrayProcessingTool.getMaxSubSumOn2([-1, 2, 3, -9])); // 5
+// console.log(ArrayProcessingTool.getMaxSubSumOn2([-1, 2, 3, -9, 11])); // 11
+// console.log(ArrayProcessingTool.getMaxSubSumOn2([-2, -1, 1, 2])); // 3
+// console.log(ArrayProcessingTool.getMaxSubSumOn2([1, 2, 3])); // 6
+// console.log(ArrayProcessingTool.getMaxSubSumOn2([100, -9, 2, -3, 5])); // 100
