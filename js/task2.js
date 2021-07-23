df0f02da568748cb73e3366a52a8d94a2b1c12b7
sashabely221100 <sashabelyy2012@gmail.com>
2021-07-23 20:23:56 +0300

    added part of datedisplayformatter functionality except regexp implementation

--- js/task2.js
+++ js/task2.js
@@ -2,38 +2,89 @@
 // do stuff
 // }
 
-let monthCheck = document.querySelector('.monthCheck');
+//! TODO добавить обработку случаев с некорректной датой
+
+
+
+
+
 
 DateDisplayFormatter = new Object();
-DateDisplayFormatter.showCurrentDate = function showCurrentDate(format, regexp = undefined) {
+let monthCheck;
+let dateInput;
+let dateInputButton;
+
+const months = [
+    'Januar',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+
+
+window.onload = function() {
+    monthCheck = document.getElementById('monthCheck');
+    dateInput = document.querySelector(".date-input");
+
+    dateInputButton = document.querySelector(".subtask2__button");
+    dateInputButton.addEventListener("click", dateClick);
+}
+
+
+DateDisplayFormatter.showCurrentDate = function showCurrentDate(dateStr, format, regexp = undefined) {
+
+    let day = dateStr.slice(0, 2);
+    let month = dateStr.slice(2, 4);
+    let year = dateStr.slice(4, 8);
+    if (monthCheck.checked) {
+
+
+        month = months[String(month)];
+
+    }
+
+
+
+
     let currentDate = new Date(); // текущая дата
 
 
-    const month = [
-        'Januar',
-        'February',
-        'March',
-        'Aprik',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-    ];
 
-    let currentDay = currentDate.getDate();
-    console.log(currentDay);
 
-    let currentMonth = currentDate.getMonth();
-    console.log(currentMonth);
+    let dateString = `${day}-${month}-${year}`;
 
-    let currentYear = currentDate.getFullYear();
-    console.log(currentYear);
-    return `${currentDay}-${month[currentMonth]}-${currentYear}`;
+    return dateString;
 
 }
 
-console.log(DateDisplayFormatter.showCurrentDate());
+
+function dateClick() {
+    dateInput.value = DateDisplayFormatter.showCurrentDate(dateInput.value);
+}
+
+
+
+
+
+
+
+
+
+
+
+// let currentDay = currentDate.getDate();
+// console.log(currentDay);
+
+// let currentMonth = currentDate.getMonth();
+// console.log(currentMonth);
+
+// let currentYear = currentDate.getFullYear();
+// console.log(currentYear);
