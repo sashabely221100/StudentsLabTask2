267e478107400c4515728261922065ba29c36929
sashabely221100 <sashabelyy2012@gmail.com>
2021-07-26 20:39:54 +0300

    added validation of the input field

--- js/task2.js
+++ js/task2.js
@@ -15,7 +15,7 @@ let dateInput;
 let dateInputButton;
 
 const months = [
-    'Januar',
+    'January',
     'February',
     'March',
     'April',
@@ -36,34 +36,57 @@ window.onload = function() {
 
     dateInputButton = document.querySelector(".subtask2__button");
     dateInputButton.addEventListener("click", dateClick);
+
 }
 
 
-DateDisplayFormatter.showCurrentDate = function showCurrentDate(dateStr, format, regexp = undefined) {
+DateDisplayFormatter.showCurrentDate = function showCurrentDate(dateStr, inputFormat = undefined, outputFormat = undefined) {
+
+    //!тут разбиение на разные переменные по запятой 
+
+    dateStr = dateStr.replace(/\D/ig, ''); //  \D  all non-digit characters between 1 and unlimited times
 
     let day = dateStr.slice(0, 2);
     let month = dateStr.slice(2, 4);
     let year = dateStr.slice(4, 8);
-    if (monthCheck.checked) {
 
 
-        month = months[String(month)];
+
+    function validateDate(date) {
+
+        if ((parseInt(date)) === NaN) {
+
+            alert("попробуйте снова");
+
+        }
+        if ((parseInt(day) > 31) || (parseInt(day) <= 0)) {
+
+            alert("Неверно введён день, попробуйте снова");
+        }
+        if ((parseInt(month) > 12) || (parseInt(month) <= 0)) {
+
+            alert("Неверно введён месяц, попробуйте снова");
+        }
+        if ((parseInt(year) > 2021) || (parseInt(year) <= 0)) {
+
+            alert("неверно введён год, попробуйте снова");
+        }
+
 
     }
 
+    validateDate(dateStr);
 
+    if (monthCheck.checked) {
+        month = String(months[Number(month) - 1]);
+    }
 
-
-    let currentDate = new Date(); // текущая дата
-
-
-
-
+    //let currentDate = new Date(); // текущая дата
     let dateString = `${day}-${month}-${year}`;
 
     return dateString;
 
-}
+};
 
 
 function dateClick() {
