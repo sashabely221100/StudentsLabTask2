f390d8e730a38628ab35b3ede381b852ebc74ae2
sashabely221100 <sashabelyy2012@gmail.com>
2021-07-28 19:42:39 +0300

    added task6 script skeleton

---
+++ js/task6.js
@@ -0,0 +1,9 @@
+let converterInput = document.getElementById("converterInput");
+
+alert(converterInput.value)
+
+console.log(+"в двоичной СИ" + converterInput.value.toString(2));
+
+console.log(converterInput.value + "в 8-ричной СИ" + converterInput.value.toString(8));
+
+console.log(converterInput.value + "в 16-ричной СИ" + converterInput.value.toString(16));
