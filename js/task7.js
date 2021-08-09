6ebf384196f4de9b7df6358f20fba259620582a1
sashabely221100 <sashabelyy2012@gmail.com>
2021-08-09 19:31:39 +0300

    refactored task 7 layout and added script skeleton

---
+++ js/task7.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    const numberButtons = document.querySelectorAll('[data-number]');
+    const calcButtons = document.querySelectorAll('[data-calculation]');
+    const equalsButton = document.querySelector('[data-equals]');
+    const deleteButton = document.querySelector('[data-delete]');
+    const allClearButton = document.querySelector('[data-all-clear]');
+    let prevValue = document.querySelector(".calculator__previous-value");
+    let currentVal = document.querySelector(".calculator__current-value");
+
+
+
+
+
+
+}
