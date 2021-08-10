bec96939088aa787a3ddfcbe5adb17c2143a42ab
sashabely221100 <sashabelyy2012@gmail.com>
2021-08-10 18:15:51 +0300

    basic functionality implemented

--- js/task7.js
+++ js/task7.js
@@ -17,51 +17,122 @@ class CachingCalculator {
     appendNum(num) {
 
 
-        if (num === '.' && this.currentOperand.includes('.')) return;
+        if (num === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + num.toString();
     }
-    chooseOperator(operation) {}
+    chooseOperator(operation) {
+
+
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.calculate();
+        }
+
+        this.operation = operation;
+        this.previousOperand = this.currentOperand; //moves cur opperand to prev opperand div
+        this.currentOperand = '';
+
+
+
+
+    }
 
 
 
     calculate() {
+        let computation;
+        const prevVal = parseFloat(this.previousOperand);
+        const currentVal = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+                computation = prevVal + currentVal;
+                break;
+            case '-':
+                computation = prevVal - currentVal;
+                break;
+            case '*':
+                computation = prevVal * currentVal;
+                break;
+            case '÷':
+                computation = prevVal / currentVal;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
 
+
+    displayNumber(num) {
+        const floatNum = parseFloat(num);
+        if (isNaN(floatNum)) return '';
+
+        return floatNum.toLocaleString('en');
     }
 
     update() {
-        this.currentOperandTextElement.innerText = this.currentOperand;
+        this.currentOperandTextElement.innerText =
+            this.displayNumber(this.currentOperand);
+        if (this.operation) {
+            this.previousOperandTextElement.innerText =
+                `${this.displayNumber(previousOperand)} ${this.operation}`
+
+        }
     }
 }
 
 
 
 
-window.onload = function() {
-    const numberButtons = document.querySelectorAll('[data-number]');
-    const calcButtons = document.querySelectorAll('[data-calculation]');
-    const equalsButton = document.querySelector('[data-equals]');
-    const deleteButton = document.querySelector('[data-delete]');
-    const allClearButton = document.querySelector('[data-all-clear]');
-    let previousOperandTextElement = document.querySelector(".calculator__previous-value");
-    let currentOperandTextElement = document.querySelector(".calculator__current-value");
 
-    const calculator = new CachingCalculator(previousOperandTextElement, currentOperandTextElement);
+const numberButtons = document.querySelectorAll('[data-number]');
+const calcButtons = document.querySelectorAll('[data-calculation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector(".calculator__previous-value");
+const currentOperandTextElement = document.querySelector(".calculator__current-value");
 
-    numberButtons.forEach(btn => {
-        btn.addEventListener('click', () => {
-            calculator.appendNum(btn.innerText);
-            calculator.update();
-        })
-    });
+const calculator = new CachingCalculator(previousOperandTextElement, currentOperandTextElement);
 
-    deleteButton.addEventListener('click', () => {
-        calculator.delete();
+
+
+numberButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        calculator.appendNum(btn.innerText);
         calculator.update();
-    });
+    })
+});
+
+
+calcButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        calculator.chooseOperator(btn.innerText);
+        calculator.update();
+    })
+});
+
+allClearButton.addEventListener('click', btn => {
+    calculator.clear();
+    calculator.update();
+})
+
+deleteButton.addEventListener('click', btn => {
+    calculator.delete();
+    calculator.update();
+});
+
+equalsButton.addEventListener('click', btn => {
+    calculator.calculate();
+    calculator.update();
+
+})
 
 
 
-}
 
 
 
