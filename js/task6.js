732855aa2a1f6252ebdd944946745fe7d7f1ba4e
sashabely221100 <sashabelyy2012@gmail.com>
2021-08-30 10:12:07 +0300

    fixed select field bug

--- js/task6.js
+++ js/task6.js
@@ -3,8 +3,8 @@ let converterInput;
 
 let converterButton;
 
-let selectOutput = "";
-let selectInput = "";
+let selectOutput;
+let selectInput;
 
 
 converterButton = document.getElementById("subTask3__button"); // btn elem
@@ -34,7 +34,7 @@ function converterInputClick() {
 }
 
 BinaryConverter.Convert = function Convert(src, from, to) {
-    //2 3 8 10 16
+
 
     function getInputRadix(InpRad) {
         return (InpRad === "bin") ? 2 : ((InpRad === "ter") ? 3 : (InpRad === "oct") ? 8 : (InpRad === "dec") ? 10 : (InpRad === "hex") ? 16 : alert("choose Input Radix"));
@@ -56,7 +56,7 @@ BinaryConverter.Convert = function Convert(src, from, to) {
 
     let convertedToDecimal = convertInputToDecimal(src, InpRadix); //after converted to decimal we can convert our input num to another base; 
 
-    function toBase(num, OutputRadix) { // only i64 numbers
+    function toBase(num, OutputRadix) {
         var keys = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
         if (!(OutputRadix >= 2 && OutputRadix <= keys.length)) throw new RangeError("toBase() radix argument must be between 2 and " + keys.length)
 
